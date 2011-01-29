051487bd721d95b9d586b9ea8f2b315297ebe3e5
Felix Geisendörfer <felix@debuggable.com>
2011-01-29 12:52:15 +0100

    Implement handler

---
+++ test/integration/scene/test-handle.js
@@ -0,0 +1,24 @@
+var common = require('../../common');
+var assert = common.assert;
+var fake = common.fake;
+
+var scene = new fake.Scene();
+
+(function testHandle() {
+  var input = {any: 'object 1'};
+  var output = {any: 'object 2'};
+  var context = {any: 'object 3'};
+
+  var callback = scene.callback();
+
+  scene
+    .expect(callback)
+    .andHandle(function(val) {
+      assert.strictEqual(this, context);
+      assert.strictEqual(val, input);
+      return output;
+    });
+
+  var r = callback.call(context, input);
+  assert.strictEqual(r, output);
+})();
