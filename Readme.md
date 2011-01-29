670793dd6c11ea453e6ef3e75fadad2d85b0646d
Felix Geisendörfer <felix@debuggable.com>
2011-01-29 14:53:54 +0100

    Constructor support

--- Readme.md
+++ Readme.md
@@ -4,7 +4,6 @@ You gotta fake it till you make it.
 
 ## Todo
 
-* Constructors
 * Useful error reporting
 * Become self-testing
 * NodeModuleScene / BrowserScriptScene?
---
+++ test/integration/scene/test-constructor.js
@@ -0,0 +1,30 @@
+var common = require('../../common');
+var assert = common.assert;
+var fake = common.fake;
+
+var scene = fake.scene();
+
+var MyClass = scene.class();
+(function testNewMyClass() {
+  scene.expect('new', MyClass)
+
+  // Should not verify initially
+  assert.throws(function() {
+    scene.verify();
+  });
+
+  var myClass = new MyClass();
+  scene.verify();
+})();
+
+(function testNewMyClass() {
+  scene.expect('new', MyClass)
+
+  // Invoking the function without new should fail
+  assert.throws(function() {
+    MyClass();
+  });
+
+  // We don't want to satisfy this expecation, so let's reset out scene
+  scene.reset();
+})();
