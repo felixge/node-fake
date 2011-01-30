d2004e84ba9a8a15cbc3a0106a8f1ffe03ff5575
Felix Geisendörfer <felix@debuggable.com>
2011-01-30 11:38:09 +0100

    Implemented time output for test runner

--- test/runner.js
+++ test/runner.js
@@ -1,20 +1,26 @@
 // Experimental, might not end up using this
 var exec = require('child_process').exec;
 
+var start = +new Date;
 exec('find -E '+__dirname+' -regex ".+/test-.+\.js"', function(err, stdout) {
   if (err) {
     throw err;
   }
 
-  stdout
+  var tests = stdout
     .split('\n')
-    .forEach(function(file) {
-      if (!file) {
-        return;
-      }
-
-      var relative = file.substr(__dirname.length);
-      console.log(relative);
-      require(file);
+    .filter(function(file) {
+      return !!file;
     });
+
+  tests.forEach(function(file) {
+    var relative = file.substr(__dirname.length);
+    console.log(relative);
+    require(file);
+  });
+
+  var duration = +new Date - start;
+
+  console.log('');
+  console.log('Executed ' + tests.length + ' tests in '+duration+' ms');
 })
