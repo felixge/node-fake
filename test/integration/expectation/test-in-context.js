46d01eb8dcc33c53fe6486abe1f503925d0c8314
Felix Geisendörfer <felix@debuggable.com>
2011-01-30 02:23:43 +0100

    Add missing context test

---
+++ test/integration/expectation/test-in-context.js
@@ -0,0 +1,31 @@
+var common = require('../../common');
+var assert = common.assert;
+var fake = common.fake;
+
+var scene = fake.scene();
+
+(function testOutOfContext() {
+  var context = {};
+  var callback = scene.callback();
+
+  scene
+    .expect(callback)
+    .inContext(context);
+
+  assert.throws(function() {
+    callback();
+  }, /context/i);
+
+  scene.reset();
+})();
+
+(function testInContext() {
+  var context = {};
+  var callback = scene.callback();
+
+  scene
+    .expect(callback)
+    .inContext(context);
+
+  callback.call(context);
+})();
