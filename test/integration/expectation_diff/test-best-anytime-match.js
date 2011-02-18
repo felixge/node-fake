b9746f3554f2317ec03eaba9aaf4b50023c018e2
Felix Geisendörfer <felix@debuggable.com>
2011-02-18 18:13:08 -0500

    Re-implement anytime matching
    Man, I really wish I could have used node-fake to write node-fake.

--- test/integration/expectation_diff/test-best-anytime-match.js
+++ test/integration/expectation_diff/test-best-anytime-match.js
@@ -7,17 +7,25 @@ var scene = fake.scene();
 (function testRightArgCountWins() {
   var callback = scene.callback();
 
-  scene
-    .expectAnytime(callback);
+  scene.expectAnytime(callback);
 
-  var arg = {any: 'object'};
+  var arg = {any: 'object 1'};
   scene
     .expectAnytime(callback)
     .withArgs(arg);
 
-  // Should be matched by the second expectation, since it has a higher score
+  scene.expectAnytime(callback);
+
+  var context = {any: 'object 2'};
+  scene
+    .expectAnytime(callback)
+    .inContext(context);
+
   callback(arg);
 
-  // Should be matched by the first expectation, since it has a lower score
+  callback.call(context);
+
+  callback();
+
   callback();
 })();
